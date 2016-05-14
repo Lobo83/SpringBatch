2c7362003c48f5854e1343a2dfb3cd4cef4568fc
Lobo83 <inmortalland83@gmail.com>
2016-05-14 10:29:18 +0200

    recuperacion de backup despues de restructuracion local

--- HolaMundoBatch/src/main/java/org/lobo/spring/batch/HolaMundoBatch/App.java
+++ HolaMundoBatch/src/main/java/org/lobo/spring/batch/HolaMundoBatch/App.java
@@ -1,8 +1,11 @@
 package org.lobo.spring.batch.HolaMundoBatch;
 
+import java.util.Date;
+
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -30,8 +33,10 @@ public class App
     	Job job = (Job) context.getBean("holaMundo");
 
     	try {
-
-    		JobExecution execution = jobLauncher.run(job, new JobParameters());
+    		//Si se hacen 2 ejecuciones identicas con los mismos parámetros Spring falla porque detecta una jobInstance ya ejecutada en estado COMPLETED
+    		//pasando la fecha de ejecución, aunque no se use, se evita este problema 
+    		JobParameters parameters = new JobParametersBuilder().addDate("fechaEjecucion", new Date()).toJobParameters();
+    		JobExecution execution = jobLauncher.run(job, parameters);
     		System.out.println("Exit Status : " + execution.getStatus());
 
     	} catch (Exception e) {
